cde11d51e9a1e4eb578d7fd6d549230503f24fa2
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-10-24 21:28:15 +0300

    Finish basic websocket server example

--- bin_server_ws/server.ml
+++ bin_server_ws/server.ml
@@ -1,5 +1,5 @@
 open Lwt.Syntax
-(* open Websocket *)
+open Websocket
 open Sexplib
 open Cohttp
 open Todo_base
@@ -17,7 +17,8 @@ type params =
     verbose: bool;
   }
 
-let src = Logs.Src.create "todo_server_ws.server"
+let src =
+  Logs.Src.create "todo_server_ws.server"
 
 let init params =
   let reporter = Log.make_reporter ~verbose:params.verbose in
@@ -35,26 +36,44 @@ let select_mode params =
            `No_password,
            `Port params.port)
 
-let on_request (_, conn) req body =
-  Logs.app ~src (fun m -> m "[CONN] %a" Sexp.pp (Connection.sexp_of_t conn));
-  let uri = Request.uri req in
-  match Uri.path uri with
+let log_frame frame =
+  let open Frame in
+  match frame.opcode with
+  | Opcode.Close ->
+     Logs.app ~src (fun m -> m "[RECV] CLOSE")
   | _ ->
-     Logs.app ~src (fun m -> m "[PATH] ?");
-     let* _ = Cohttp_lwt.Body.drain_body body in
-     let body = Sexp.to_string_hum (Request.sexp_of_t req) in
-     let* res = Server.respond_string ~status:`Not_found ~body () in
-     Lwt.return @@ `Response res
+     let msg = frame.content in
+     Logs.app ~src (fun m -> m "[RECV] %s" msg)
+
+let on_request ~req ~body =
+  let open Websocket_cohttp_lwt in
+  let* _ = Cohttp_lwt.Body.drain_body body in
+  let* (res, out) = upgrade_connection req log_frame in
+  let frame = Frame.create ~content:"Hello world" () in
+  let* _ = Lwt.wrap1 out (Some frame) in
+  Lwt.return res
 
 let on_close (_, conn) =
   Logs.app ~src (fun m ->
       m "[SERV] connection %a closed" Sexp.pp (Connection.sexp_of_t conn))
 
+let handle (_, conn) req body =
+  Logs.app ~src (fun m -> m "[CONN] %a" Sexp.pp (Connection.sexp_of_t conn));
+  let path = Uri.path (Request.uri req) in
+  Logs.app ~src (fun m -> m "[PATH] %s" path);
+  match path with
+  | "/" ->
+     on_request ~req ~body
+  | _ ->
+     let body = Sexp.to_string_hum @@ Request.sexp_of_t req in
+     let* res = Server.respond_string ~status:`Not_found ~body () in
+     Lwt.return @@ `Response res
+
 let serve params =
   let mode = select_mode params in
   let srv =
     Server.make_response_action
-      ~callback:on_request
+      ~callback:handle
       ~conn_closed:on_close
       ()
   in
