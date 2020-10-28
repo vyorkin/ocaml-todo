b768e4e21bf20d06ab1b2d2fbfca66620a3d9de2
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2020-10-28 20:53:33 +0300

    Keep working on metrics

--- lib_base/metrics.ml
+++ lib_base/metrics.ml
@@ -1,30 +1,45 @@
 open Prometheus
 
-let namespace = "todo"
 
 module Http = struct
-  module Request = struct
-    let subsystem = "http.request"
+  (** TODO: use functor instead *)
+  module H = DefaultHistogram
 
-    let duration =
-      Counter.v_label
+  let namespace = "todo.http"
+
+  let label_names =
+    [ "method";
+      "path"
+    ]
+
+  module Request = struct
+    let subsystem = "request"
+
+    let duration_labels =
+      H.v_labels
+        ~label_names
         ~help:"Duration of HTTP request (sec)"
-        ~label_name:"request"
         ~namespace
         ~subsystem
         "duration.seconds"
 
-    let size =
-      Counter.v_label
+    let size_labels =
+      H.v_labels
+        ~label_names
         ~help:"Size of HTTP request (bytes)"
-        ~label_name:"request"
         ~namespace
         ~subsystem
         "size.bytes"
+
+    let duration ~meth ~path =
+      H.labels duration_labels [ meth; path ]
+
+    let size ~meth ~path =
+      H.labels size_labels [ meth; path ]
   end
 
   module Response = struct
-    let subsystem = "http.response"
+    let subsystem = "response"
 
     let size =
       Counter.v_label
